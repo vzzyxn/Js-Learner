00ea893240a3f37d1cc99145d0215a8efc1c00d5
Vignesh <acevicky5@gmail.com>
2021-11-25 21:36:36 +0530

    Add first commit

---
+++ intro.js
@@ -0,0 +1,13 @@
+// Learn Javascript // this is an single line comment 
+
+/* and this is an double line comment
+    
+         Introduction to Javascript */
+
+// 1. Datatypes
+console.log(typeof 'Abc') // String 
+console.log(typeof  12) // int 
+console.log(typeof true) // boolean
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined 
+
