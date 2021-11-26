a5f2f41918879fb24aad50057b078d7b5367f87b
Vignesh <acevicky5@gmail.com>
2021-11-26 18:53:52 +0530

    New commit

--- intro.js
+++ intro.js
@@ -11,3 +11,71 @@ console.log(typeof true) // boolean
 console.log(typeof null) // object
 console.log(typeof undefined) // undefined 
 
+// 2.primitive data type
+let one = 'Javascript'
+x = one[0]
+console.log(x) // output = J 
+
+let two = 'Javascript'
+y = two[1] 
+console.log(y) // output = a
+
+// compare two values 
+// example 1
+let var1 = 1
+let var2 = 1
+
+console.log(var1 == var2) // output = true
+
+//example 2
+let str1 = 'Javascript'
+let str2 = 'Python'
+
+console.log(str1 == str2) // output = false 
+
+//example 3
+let bool1 = true
+let bool2 = false
+
+console.log(bool1 == bool2) //output = false 
+
+
+// 3.non-primitive data type
+// so here in this example we can change the first postion to another value
+let num1 = [5,20,30]
+num1[0] = 10 
+console.log(num1) // ouput = [10,20,30] 
+
+///_______________________________________________________
+/// IMPORTANT = we cannot compare non primitive data types/
+///    we can do it but in another way as mentiioned below 
+///______________________________________________________/
+
+//example-1 
+let number1 = [10,20,30]
+let number2 = number1 
+console.log(number2) //output = [10,20,30]
+//example-2 
+let compare1 = {
+    name:'vicky',
+    role:'coding',
+    fav:'pyhton'
+}
+let compare2 = compare1
+console.log(compare1 == compare2)  // true 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
