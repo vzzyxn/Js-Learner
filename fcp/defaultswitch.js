161036f32ee427faf091be0411526bc3137b5354
Vignesh <acevicky5@gmail.com>
2022-03-23 18:12:38 +0530

    adding default

---
+++ fcp/defaultswitch.js
@@ -0,0 +1,20 @@
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val) {
+    case "a":
+      answer = "apple";
+    break;
+
+    case "b":
+      answer = "bird";
+    break;
+
+    case "c":
+      answer = "cat";
+    break;
+
+    default:
+      answer = "stuff";
+      break;
+}
