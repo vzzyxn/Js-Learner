cbe385ed003ffd74063a1bf5c0f191dea65b0635
Vignesh <acevicky5@gmail.com>
2022-04-07 21:51:08 +0530

    accessobject(.)

---
+++ fcp/accessobj.js
@@ -0,0 +1,8 @@
+// accesing object properties with dot notation
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+const hatValue = testObj.hat;
+const shirtValue = testObj.shirt;
